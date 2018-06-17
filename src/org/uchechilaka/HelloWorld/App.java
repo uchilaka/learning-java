486d60589a2c9924e84397277cb24aa184f249fa
Uchenna Chilaka <uche@larcity.com>
2018-06-17 10:47:45 -0400

    latest updates. Stopping

--- src/org/uchechilaka/HelloWorld/App.java
+++ src/org/uchechilaka/HelloWorld/App.java
@@ -1,6 +1,17 @@
 package org.uchechilaka.HelloWorld;
 
 public class App {
+    public static void doSomeUselessStuff() {
+        int x = 10;
+        int y = 5;
+        int z = x + y;
+        x = z - y;
+        y = x + z;
+        z = 2 * z;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+    }
     public static void main(String[] args) {
         // System.out.println("Hello World!");
 //        Experiments exp = new Experiments();
@@ -16,8 +27,15 @@ public class App {
 //            System.out.println(ex.toString());
 //        }
 
-        VacationPlanner agency = new VacationPlanner();
-        agency.begin();
+//        System.out.println("10 plus 20 is " + 10 + 20);
+//        System.out.println("1 + 3 / 2 - 7 % 3 = " + ( ((3/2) - (7 % 3)) + 1 ));
+////        System.out.println("1 + 3 / 2 - 7 % 3 = " + (((3/2) + 1 - 7) % 3));
+//        System.out.println("(Computed) = " + (1 + 3 / 2 - 7 % 3));
+
+        doSomeUselessStuff();
+
+//        VacationPlanner agency = new VacationPlanner();
+//        agency.begin();
 
     }
 }
